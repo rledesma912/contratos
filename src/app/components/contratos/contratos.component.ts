fbf9d976fe22231b2c70b160ad801adb7ad06502
unknown <hledesma@aftic39.cnc.inter>
2017-12-04 17:00:50 -0300

    Terminando los formularios de parrafos y contratos. v1.0

--- src/app/components/contratos/contratos.component.ts
+++ src/app/components/contratos/contratos.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contratos',
-  templateUrl: './contratos.component.html'
+  templateUrl: './contratos.component.html',
+  styleUrls: ['../../../forms.css']
 })
 export class ContratosComponent implements OnInit {
 
